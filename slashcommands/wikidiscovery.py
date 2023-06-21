5d9fa66e5c52e7aa39c95ffd96d2e69f5f6e9e86
TruongKhoaUI <125333267+TruongKhoaUI@users.noreply.github.com>
2023-06-21 23:59:49 +0000

    Removed unused local assignment

--- slashcommands/wikidiscovery.py
+++ slashcommands/wikidiscovery.py
@@ -21,7 +21,7 @@ class WikidiscoveryCommandSlash(commands.Cog):
       embed = discord.Embed(title=f"Searched for {search}", description=f"{e}", color=0x3f48cc)
       await interaction.response.send_message(embed=embed)
     # This message will be sent if the keyword you provived is not available on Wikipedia
-    except wikipedia.exceptions.PageError as e:
+    except wikipedia.exceptions.PageError:
       embed = discord.Embed(title=f"Searched for {search}", description="The information you provided doesn't match any query.", color=0x3f48cc)
       await interaction.response.send_message(embed=embed)
 
