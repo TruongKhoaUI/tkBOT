9dc83a7afe2412f10212b26caefe12786a601499
TruongKhoaUI <125333267+TruongKhoaUI@users.noreply.github.com>
2023-06-22 01:08:20 +0000

    Fixed the reversed code for sent and edited

--- botlogsmessage/messagesent.py
+++ botlogsmessage/messagesent.py
@@ -7,30 +7,26 @@ class Messagesent(commands.Cog):
     self.bot = bot
 
   @commands.Cog.listener()
-  async def on_message_edit(self, before, after):
-    if before.author == self.bot.user:
+  async def on_message(self, message):
+    if message.author == self.bot.user:
       return
     # Get channel ID
-    channel = self.bot.get_channel(int(os.getenv('bot_channel_edited_message')))
+    channel = self.bot.get_channel(int(os.getenv('bot_channel_sent_message')))
     embed = discord.Embed(color=0x3f48cc)
-    embed.set_author(name=before.author, icon_url=before.author.display_avatar.url)
-    attachment_urls = ' | '.join([attachment.url for attachment in before.attachments])
+    embed.set_author(name=message.author, icon_url=message.author.display_avatar.url)
+    attachment_urls = ' | '.join([attachment.url for attachment in message.attachments])
     # Show embed message
-    if len(before.embeds) > 0:
-      edited_content = "**Embed message**"
+    if len(message.embeds) > 0:
+      content = "**Embed message**"
     else:
-      edited_content = before.content
-    if len(after.embeds) > 0:
-      edited_content = "**Embed message**"
-    else:
-      edited_content = after.content
+      content = message.content
     if attachment_urls:
       attachment_text = f"｜[attachment]({attachment_urls})"
     else:
       attachment_text = ""
-    # Send the message when they have edited the server with this bot added
-    if not isinstance(before.channel, discord.channel.DMChannel):
-      embed.add_field(name="Message edited", value=f"- **Original content:** {before.content}{attachment_text}\n- **Edited content:** {edited_content}{attachment_text}\n- **Edited time:** {after.created_at.strftime('%Y-%m-%d %H:%M:%S')}\n- **Edited at:** {before.guild.name}")
+    # Send the message when they have sent from the server with this bot added
+    if not isinstance(message.channel, discord.channel.DMChannel):
+      embed.add_field(name="Message sent", value=f"- **Content:** {content}{attachment_text}\n- **Sent time:** {message.created_at.strftime('%Y-%m-%d %H:%M:%S')}\n- **Sent at:** {message.guild.name}")
       await channel.send(embed=embed)
 
 def setup(bot):
