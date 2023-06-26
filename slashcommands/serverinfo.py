374e7da8a7371ef98a2998638681bd49a8228855
TruongKhoaUI <125333267+TruongKhoaUI@users.noreply.github.com>
2023-06-26 03:15:24 +0000

    Added verification level

--- slashcommands/serverinfo.py
+++ slashcommands/serverinfo.py
@@ -13,7 +13,7 @@ class ServerinfoCommandSlash(commands.Cog):
     if ctx.guild:
       current_server = ctx.guild
       # View the guild you're in
-      embed = discord.Embed(title=f"{current_server.name}", description=f"\n- 👤｜Server Owner: {current_server.owner}\n- 🆔｜Server ID: {current_server.id}\n- 📅｜Created on: {current_server.created_at.strftime('%a %d %B %Y, %I:%M %p')}\n- 👥｜Members in total: {current_server.member_count} member(s)\n- 🥇｜Roles in total: {len(current_server.roles)} role(s)\n- 💬｜Channels in total: {len(current_server.channels)} channel(s)\n- 🔊｜Voice channels in total: {len(current_server.voice_channels)} voice channel(s)", color=0x3f48cc)
+      embed = discord.Embed(title=f"{current_server.name}", description=f"\n- 👤｜Server Owner: {current_server.owner}\n- 🆔｜Server ID: {current_server.id}\n- 📅｜Created on: {current_server.created_at.strftime('%a %d %B %Y, %I:%M %p')}\n- 👥｜Members in total: {current_server.member_count} member(s)\n- 🥇｜Roles in total: {len(current_server.roles)} role(s)\n- 💬｜Channels in total: {len(current_server.channels)} channel(s)\n- 🔊｜Voice channels in total: {len(current_server.voice_channels)} voice channel(s)\n- ✅｜Verification Level: {current_server.verification_level}", color=0x3f48cc)
       # View the guild but if the server icon is empty, it won't show any images
       if current_server.icon is not None:
         embed.set_thumbnail(url=current_server.icon.url)
@@ -21,8 +21,7 @@ class ServerinfoCommandSlash(commands.Cog):
         embed.set_thumbnail(url=None)
     # If you are in DM, it will send this message
     else:
-      embed = discord.Embed(title="Server Information", description="", color=0x3f48cc)
-      embed.add_field(name="There isn't any server now...", value="No server joined, or currently in DM.", inline=False)
+      embed = discord.Embed(title="Server Information", description="You are not on any server. Try to go to any server and use this command.", color=0x3f48cc)
     await interaction.followup.send(embed=embed)
 
 def setup(bot):
