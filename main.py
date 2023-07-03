a3c52fa30d265857cf613110d2b917ee7aec4f7c
TruongKhoaUI <125333267+TruongKhoaUI@users.noreply.github.com>
2023-07-03 11:31:34 +0000

    Arranged the code

--- main.py
+++ main.py
@@ -6,25 +6,44 @@ import os
 from flask import Flask, render_template
 from threading import Thread
 from discord.ext import commands
-# Import commands
-from commands.prefixcommands.ban import BanCommand; from commands.slashcommands.ban import BanCommandSlash
-from commands.prefixcommands.botinfo import BotinfoCommand; from commands.slashcommands.botinfo import BotinfoCommandSlash
-from commands.prefixcommands.echo import EchoCommand; from commands.slashcommands.echo import EchoCommandSlash
-from commands.prefixcommands.help import HelpCommand; from commands.slashcommands.help import HelpCommandSlash
-from commands.prefixcommands.image import ImageCommand; from commands.slashcommands.image import ImageCommandSlash
-from commands.prefixcommands.kick import KickCommand; from commands.slashcommands.kick import KickCommandSlash
-from commands.prefixcommands.leaderboard import LeaderboardCommand; from commands.slashcommands.leaderboard import LeaderboardCommandSlash
-from commands.prefixcommands.pfp import PfpCommand; from commands.slashcommands.pfp import PfpCommandSlash
-from commands.prefixcommands.ping import PingCommand; from commands.slashcommands.ping import PingCommandSlash
-from commands.prefixcommands.resetpoints import ResetpointsCommand; from commands.slashcommands.resetpoints import ResetpointsCommandSlash
-from commands.prefixcommands.serverinfo import ServerinfoCommand; from commands.slashcommands.serverinfo import ServerinfoCommandSlash
-from commands.prefixcommands.timeout import TimeoutCommand; from commands.slashcommands.timeout import TimeoutCommandSlash
-from commands.prefixcommands.userinfo import UserinfoCommand; from commands.slashcommands.userinfo import UserinfoCommandSlash
-from commands.prefixcommands.warn import WarnCommand; from commands.slashcommands.warn import WarnCommandSlash
-from commands.prefixcommands.wikidiscovery import WikidiscoveryCommand; from commands.slashcommands.wikidiscovery import WikidiscoveryCommandSlash
 # Import chatlog code
-from commands.botlogsmessage.messagesent import Messagesent; from commands.botlogsmessage.messageedit import Messageedit; from commands.botlogsmessage.messagedelete import Messagedelete
-from commands.botlogsmessage.svjoined import Serverjoin; from commands.botlogsmessage.svkicked import Serverkick
+from commands.botlogsmessage.messagesent import Messagesent
+from commands.botlogsmessage.messageedit import Messageedit
+from commands.botlogsmessage.messagedelete import Messagedelete
+from commands.botlogsmessage.svjoined import Serverjoin
+from commands.botlogsmessage.svkicked import Serverkick
+# Import prefix commands
+from commands.prefixcommands.ban import BanCommand
+from commands.prefixcommands.botinfo import BotinfoCommand
+from commands.prefixcommands.echo import EchoCommand
+from commands.prefixcommands.help import HelpCommand
+from commands.prefixcommands.image import ImageCommand
+from commands.prefixcommands.kick import KickCommand
+from commands.prefixcommands.leaderboard import LeaderboardCommand
+from commands.prefixcommands.pfp import PfpCommand
+from commands.prefixcommands.ping import PingCommand
+from commands.prefixcommands.resetpoints import ResetpointsCommand
+from commands.prefixcommands.serverinfo import ServerinfoCommand
+from commands.prefixcommands.timeout import TimeoutCommand
+from commands.prefixcommands.userinfo import UserinfoCommand
+from commands.prefixcommands.warn import WarnCommand
+from commands.prefixcommands.wikidiscovery import WikidiscoveryCommand
+# Import slash commands
+from commands.slashcommands.ban import BanCommandSlash
+from commands.slashcommands.botinfo import BotinfoCommandSlash
+from commands.slashcommands.echo import EchoCommandSlash
+from commands.slashcommands.help import HelpCommandSlash
+from commands.slashcommands.image import ImageCommandSlash
+from commands.slashcommands.kick import KickCommandSlash
+from commands.slashcommands.leaderboard import LeaderboardCommandSlash
+from commands.slashcommands.pfp import PfpCommandSlash
+from commands.slashcommands.ping import PingCommandSlash
+from commands.slashcommands.resetpoints import ResetpointsCommandSlash
+from commands.slashcommands.serverinfo import ServerinfoCommandSlash
+from commands.slashcommands.timeout import TimeoutCommandSlash
+from commands.slashcommands.userinfo import UserinfoCommandSlash
+from commands.slashcommands.warn import WarnCommandSlash
+from commands.slashcommands.wikidiscovery import WikidiscoveryCommandSlash
 
 # Bot settings
 # Commands
@@ -54,25 +73,44 @@ t.start()
 @bot.event  # Connected status
 async def on_ready():
   await bot.change_presence(activity=discord.Game(name='tk!help and /help'), status=discord.Status.online)
-  # Import commands
-  await bot.add_cog(BanCommand(bot)); await bot.add_cog(BanCommandSlash(bot))
-  await bot.add_cog(BotinfoCommand(bot));  await bot.add_cog(BotinfoCommandSlash(bot))
-  await bot.add_cog(EchoCommand(bot)); await bot.add_cog(EchoCommandSlash(bot))
-  await bot.add_cog(HelpCommand(bot)); await bot.add_cog(HelpCommandSlash(bot))
-  await bot.add_cog(ImageCommand(bot)); await bot.add_cog(ImageCommandSlash(bot))
-  await bot.add_cog(KickCommand(bot)); await bot.add_cog(KickCommandSlash(bot))
-  await bot.add_cog(LeaderboardCommand(bot)); await bot.add_cog(LeaderboardCommandSlash(bot))
-  await bot.add_cog(PfpCommand(bot)); await bot.add_cog(PfpCommandSlash(bot))
-  await bot.add_cog(PingCommand(bot)); await bot.add_cog(PingCommandSlash(bot))
-  await bot.add_cog(ResetpointsCommand(bot)); await bot.add_cog(ResetpointsCommandSlash(bot))
-  await bot.add_cog(ServerinfoCommand(bot)); await bot.add_cog(ServerinfoCommandSlash(bot))
-  await bot.add_cog(TimeoutCommand(bot)); await bot.add_cog(TimeoutCommandSlash(bot))
-  await bot.add_cog(UserinfoCommand(bot)); await bot.add_cog(UserinfoCommandSlash(bot))
-  await bot.add_cog(WarnCommand(bot)); await bot.add_cog(WarnCommandSlash(bot))
-  await bot.add_cog(WikidiscoveryCommand(bot)); await bot.add_cog(WikidiscoveryCommandSlash(bot))
-  # Import chatlog code
-  await bot.add_cog(Messagesent(bot)); await bot.add_cog(Messageedit(bot)); await bot.add_cog(Messagedelete(bot))
-  await bot.add_cog(Serverjoin(bot)); await bot.add_cog(Serverkick(bot))
+  # Register chatlog code
+  await bot.add_cog(Messagesent(bot))
+  await bot.add_cog(Messageedit(bot))
+  await bot.add_cog(Messagedelete(bot))
+  await bot.add_cog(Serverjoin(bot))
+  await bot.add_cog(Serverkick(bot))  
+  # Register prefix commands
+  await bot.add_cog(BanCommand(bot))
+  await bot.add_cog(BotinfoCommand(bot))
+  await bot.add_cog(EchoCommand(bot))
+  await bot.add_cog(HelpCommand(bot))
+  await bot.add_cog(ImageCommand(bot))
+  await bot.add_cog(KickCommand(bot))
+  await bot.add_cog(LeaderboardCommand(bot))
+  await bot.add_cog(PfpCommand(bot))
+  await bot.add_cog(PingCommand(bot))
+  await bot.add_cog(ResetpointsCommand(bot))
+  await bot.add_cog(ServerinfoCommand(bot))
+  await bot.add_cog(TimeoutCommand(bot))
+  await bot.add_cog(UserinfoCommand(bot))
+  await bot.add_cog(WarnCommand(bot))
+  await bot.add_cog(WikidiscoveryCommand(bot))
+  # Register slash commands
+  await bot.add_cog(BanCommandSlash(bot))  
+  await bot.add_cog(BotinfoCommandSlash(bot))  
+  await bot.add_cog(EchoCommandSlash(bot))  
+  await bot.add_cog(HelpCommandSlash(bot))  
+  await bot.add_cog(ImageCommandSlash(bot))
+  await bot.add_cog(KickCommandSlash(bot))
+  await bot.add_cog(LeaderboardCommandSlash(bot))
+  await bot.add_cog(PfpCommandSlash(bot))
+  await bot.add_cog(PingCommandSlash(bot))  
+  await bot.add_cog(ResetpointsCommandSlash(bot))  
+  await bot.add_cog(ServerinfoCommandSlash(bot))  
+  await bot.add_cog(TimeoutCommandSlash(bot))  
+  await bot.add_cog(UserinfoCommandSlash(bot))  
+  await bot.add_cog(WarnCommandSlash(bot))  
+  await bot.add_cog(WikidiscoveryCommandSlash(bot))  
   # If this text does not appear in print command, that means the bot failed to connect
   start_time = datetime.datetime.utcnow()
   session_id = bot._connection._get_websocket(shard_id=None).session_id
